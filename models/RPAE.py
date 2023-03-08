2f801b83826140b297bb0275a0da450bdf60aa37
Benjamin Midtvedt <benmid@student.chalmers.se>
2023-03-08 11:26:00 +0100

    first "working"

--- models/RPAE.py
+++ models/RPAE.py
@@ -4,12 +4,12 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 
 import torchvision.models as models
-from torchvision.models.detection import (
-    rpn, anchor_utils, transform, backbone_utils
-)
+from torchvision.models.detection import rpn, anchor_utils, transform, backbone_utils
 from torchvision import ops
 
 models.detection.FasterRCNN
+
+
 class RPAE(pl.LightningModule):
     """Region Proposal Autoencoder"""
 
@@ -19,10 +19,10 @@ class RPAE(pl.LightningModule):
         rpn_bg_iou_thresh=0.3,
         rpn_batch_size_per_image=256,
         rpn_positive_fraction=0.5,
-        rpn_pre_nms_top_n_train=2000,
-        rpn_pre_nms_top_n_test=1000,
-        rpn_post_nms_top_n_train=2000,
-        rpn_post_nms_top_n_test=1000,
+        rpn_pre_nms_top_n_train=100,
+        rpn_pre_nms_top_n_test=100,
+        rpn_post_nms_top_n_train=100,
+        rpn_post_nms_top_n_test=100,
         rpn_nms_thresh=0.7,
         rpn_score_thresh=0.0,
     ):
@@ -32,7 +32,7 @@ class RPAE(pl.LightningModule):
         )
 
         out_channels = self.backbone.out_channels
-        
+
         anchor_sizes = ((32,), (64,), (128,), (256,), (512,))
         aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
         anchor_generator = anchor_utils.AnchorGenerator(
@@ -84,8 +84,6 @@ class RPAE(pl.LightningModule):
         # get image features
         features = self.backbone(images.tensors)
         features = list(features.values())
-        print(len(features))
-        print([f.shape for f in features])
 
         # get proposals
         objectness, pred_bbox_deltas = self.rpn.head(features)
@@ -100,42 +98,152 @@ class RPAE(pl.LightningModule):
             objectness, pred_bbox_deltas
         )
 
-        proposals = self.rpn.box_coder.decode(pred_bbox_deltas.detach(), anchors)
+        proposals = self.rpn.box_coder.decode(pred_bbox_deltas, anchors)
         proposals = proposals.view(num_images, -1, 4)
-        boxes, scores = self.rpn.filter_proposals(
+        boxes, scores = self.filter_proposals(
             proposals, objectness, images.image_sizes, num_anchors_per_level
         )
-        print(
-            images.image_sizes,
-            original_image_sizes,
-        )
+
         scaled_boxes = []
         for i, boxtensor in enumerate(boxes):
             original_image_size = original_image_sizes[i]
             image_size = images.image_sizes[i]
-            scale_x = float(original_image_size[1]) / float(image_size[1])
-            scale_y = float(original_image_size[0]) / float(image_size[0])
+            scale_x = original_image_size[1] / image_size[1]
+            scale_y = original_image_size[0] / image_size[0]
 
             scaled_boxtensor = boxtensor * torch.tensor(
                 [scale_x, scale_y, scale_x, scale_y], device=boxtensor.device
             )
 
-            scaled_boxes.append(
-                scaled_boxtensor
-            )
-
+            scaled_boxes.append(scaled_boxtensor)
 
         return scaled_boxes, scores
 
     def training_step(self, images, _batch_idx):
+        images = images.float()
         boxes, scores = self(images)
 
-        pooled_boxes = ops.roi_align(
-            images, boxes, output_size=(1, 1), spatial_scale=1.0 / 16
-        )
-        pooled_boxes = pooled_boxes.squeeze(3).squeeze(2)
-        # loss is the cross entropy between the pooled boxes and the scores 
+        n_reconstruct = [1, 10, 50, 100]
+        losses = {f"loss_top_{n}": 0.0 for n in n_reconstruct}
+
+        for batch_idx in range(images.shape[0]):
+
+            image = images[batch_idx : batch_idx + 1]
+            pooled_boxes = ops.roi_align(
+                images[batch_idx : batch_idx + 1],
+                boxes[batch_idx : batch_idx + 1],
+                output_size=(28, 28),
+                spatial_scale=1.0,
+            )
+
+            reconstructed_images = torch.zeros_like(image).to(image.device)
+            image_weights = torch.zeros_like(image).to(image.device) + 1e-8
+            for i in range(boxes[batch_idx].shape[0]):
+
+                box = boxes[batch_idx][i]
+                pooled_box = pooled_boxes[i]
+                scale_x = 256 / (box[2] - box[0])
+                scale_y = 256 / (box[3] - box[1])
+                half_box_width = (box[2] - box[0]) / 2
+                half_box_height = (box[3] - box[1]) / 2
+                translate_x = -((box[0] + half_box_width) / 128 - 1) * scale_x
+                translate_y = -((box[1] + half_box_height) / 128 - 1) * scale_y
+                scale_and_translate = torch.Tensor(
+                    [
+                        [scale_x, 0, translate_x],
+                        [0, scale_y, translate_y],
+                    ]
+                ).to(image.device)
+
+                grid = F.affine_grid(scale_and_translate[None], image.shape)
+                resized_box = F.grid_sample(pooled_box[None], grid)[0]
+
+                where_box = resized_box != 0
+                # Add the resized box to the image
+                reconstructed_images = (
+                    reconstructed_images + resized_box[None] * scores[batch_idx][i]
+                )
+                image_weights = image_weights + where_box[None] * scores[batch_idx][i]
+
+                if i + 1 in n_reconstruct:
+                    losses[f"loss_top_{i + 1}"] = losses[
+                        f"loss_top_{i + 1}"
+                    ] + F.mse_loss(
+                        reconstructed_images / image_weights,
+                        images[batch_idx : batch_idx + 1],
+                    )
+
         loss = 0
-        for i in range(len(pooled_boxes)):
-            loss += torch.log(pooled_boxes[i, 0]) * scores[i]
-        return {"loss": 0}
+        for idx, l in enumerate(losses.values()):
+            if idx == 0:
+                loss += l * 0.1
+            else:
+                loss += l
+        losses["loss"] = loss
+
+        self.log_dict(losses, on_step=True)
+
+        return losses
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
+
+    def filter_proposals(
+        self,
+        proposals,
+        objectness,
+        image_shapes,
+        num_anchors_per_level,
+    ):
+
+        num_images = proposals.shape[0]
+        device = proposals.device
+        # do not backprop through objectness
+        objectness = objectness
+        objectness = objectness.reshape(num_images, -1)
+
+        levels = [
+            torch.full((n,), idx, dtype=torch.int64, device=device)
+            for idx, n in enumerate(num_anchors_per_level)
+        ]
+        levels = torch.cat(levels, 0)
+        levels = levels.reshape(1, -1).expand_as(objectness)
+
+        # select top_n boxes independently per level before applying nms
+        top_n_idx = self.rpn._get_top_n_idx(objectness, num_anchors_per_level)
+
+        image_range = torch.arange(num_images, device=device)
+        batch_idx = image_range[:, None]
+
+        objectness = objectness[batch_idx, top_n_idx]
+        levels = levels[batch_idx, top_n_idx]
+        proposals = proposals[batch_idx, top_n_idx]
+
+        objectness_prob = torch.sigmoid(objectness)
+
+        final_boxes = []
+        final_scores = []
+        for boxes, scores, lvl, img_shape in zip(
+            proposals, objectness_prob, levels, image_shapes
+        ):
+            boxes = ops.boxes.clip_boxes_to_image(boxes, img_shape)
+
+            # remove small boxes
+            keep = ops.boxes.remove_small_boxes(boxes, self.rpn.min_size)
+            boxes, scores, lvl = boxes[keep], scores[keep], lvl[keep]
+
+            # remove low scoring boxes
+            # use >= for Backwards compatibility
+            keep = torch.where(scores >= self.rpn.score_thresh)[0]
+            boxes, scores, lvl = boxes[keep], scores[keep], lvl[keep]
+
+            # non-maximum suppression, independently done per level
+            keep = ops.boxes.batched_nms(boxes, scores, lvl, self.rpn.nms_thresh)
+
+            # keep only topk scoring predictions
+            keep = keep[: self.rpn.post_nms_top_n()]
+            boxes, scores = boxes[keep], scores[keep]
+
+            final_boxes.append(boxes)
+            final_scores.append(scores)
+        return final_boxes, final_scores
--- testing.py
+++ testing.py
@@ -2,16 +2,17 @@
 
 from models.RPAE import RPAE
 
-model = RPAE()
-print(model)
+
 #%%
 import deeptrack as dt
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+torch.autograd.set_detect_anomaly(True)
+model = RPAE()
 image = dt.Value(lambda: np.zeros((3, 256, 256)))
+
 
 def add_rectangle(x, y, w, h):
     def inner(image):
@@ -19,70 +20,117 @@ def add_rectangle(x, y, w, h):
         _y = int(y)
         _w = int(w)
         _h = int(h)
-        image[:, _x:_x+_w, _y:_y+_h] = 1
+        image[:, _x : _x + _w, _y : _y + _h] = 1
         return image
+
     return inner
 
-image >>= dt.Lambda(
-    add_rectangle,
-    x=lambda: np.random.randint(0, 256),
-    y=lambda: np.random.randint(0, 256),
-    w=lambda: np.random.randint(20, 50),
-    h=lambda: np.random.randint(20, 50), 
-) ^ 10
 
-im = image()
+image >>= (
+    dt.Lambda(
+        add_rectangle,
+        x=lambda: np.random.randint(0, 256),
+        y=lambda: np.random.randint(0, 256),
+        w=lambda: np.random.randint(20, 50),
+        h=lambda: np.random.randint(20, 50),
+    )
+    ^ 10
+)
 
-plt.imshow(im[0])
+images = [image.update()()._value.astype(float) for _ in range(256)]
 
 # image = torch.zeros(1, 3, 256, 256)
 
-boxes, scores = model(torch.Tensor(im[None]))
+# boxes, scores = model(torch.Tensor(im[None]))
 
 # %%
-# For each box, crop the image 
+import pytorch_lightning as pl
+
+
+trainer = pl.Trainer(gpus=1, max_epochs=1)
+trainer.fit(model, torch.utils.data.DataLoader(images, batch_size=4))
+
+
+# %%
+# For each box, crop the image
 import torch.nn.functional as F
 from torchvision.ops import roi_align
 
-crops = []
-# for box in boxes[0][:1]:
-    # size = (int(box[3] - box[1]), int(box[2] - box[0]))
-    # print(box)
-crops = roi_align(
-    torch.Tensor(im[None]),
-    boxes,
-    (32, 32),
-)
-
-print(crops.shape)
-
-        
-# model.training_step(image, 0)
-
-# %%
-
-# apply nms to the boxes
 from torchvision.ops import nms
+from torchvision import ops
 
-keep = nms(boxes[0], scores[0], 0.5)
+im = image.update()()
+images = torch.Tensor(im[None])
+boxes, scores = model(images)
+# print(boxes[0].shape)
+pooled_boxes = ops.roi_align(images, boxes, output_size=(28, 28), spatial_scale=1.0)
 
-kept_boxes = boxes[keep]
-kept_scores = scores[keep]
+# n_reconstruct =
+print(boxes[0].shape)
 
-kept_boxes = [kept_boxes]
-kept_scores = [kept_scores]
+reconstructed_images = torch.zeros_like(images)
+image_weights = torch.zeros_like(images) + 1e-8
+for i in range(pooled_boxes.shape[0]):
 
-plt.imshow(im[0])
-for box in kept_boxes[0]:
-    box = box 
+    box = boxes[0][i]
+    pooled_box = pooled_boxes[i]
 
-    plt.gca().add_patch(plt.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], fill=False, edgecolor='red', linewidth=1))
+    # Resize and add the pooled box to the image using grid_sample
+    scale_x = 256 / (box[2] - box[0])
+    scale_y = 256 / (box[3] - box[1])
 
+    half_box_width = (box[2] - box[0]) / 2
+    half_box_height = (box[3] - box[1]) / 2
+    translate_x = -((box[0] + half_box_width) / 128 - 1) * scale_x
+    translate_y = -((box[1] + half_box_height) / 128 - 1) * scale_y
+    scale_and_translate = torch.Tensor(
+        [
+            [scale_x, 0, translate_x],
+            [0, scale_y, translate_y],
+        ]
+    )
 
+    grid = F.affine_grid(scale_and_translate[None], images.shape)
+    resized_box = F.grid_sample(pooled_box[None], grid)[0]
 
+    where_box = resized_box != 0
+    # Add the resized box to the image
+    reconstructed_images[0] += resized_box
+    image_weights[0] += where_box * scores[0][i]
+
+reconstructed_images /= image_weights
+
+plt.figure(figsize=(10, 10))
+plt.subplot(1, 2, 1)
+plt.imshow(images[0].detach().numpy().transpose(1, 2, 0))
+for box in boxes[0]:
+    box = box.detach().numpy()
+
+    plt.gca().add_patch(
+        plt.Rectangle(
+            (box[0], box[1]),
+            box[2] - box[0],
+            box[3] - box[1],
+            fill=False,
+            edgecolor="red",
+            linewidth=1,
+        )
+    )
+plt.subplot(1, 2, 2)
+plt.imshow(reconstructed_images[0].detach().numpy().transpose(1, 2, 0))
+for box in boxes[0]:
+    box = box.detach().numpy()
+
+    plt.gca().add_patch(
+        plt.Rectangle(
+            (box[0], box[1]),
+            box[2] - box[0],
+            box[3] - box[1],
+            fill=False,
+            edgecolor="red",
+            linewidth=1,
+        )
+    )
 # %%
-torch.max(boxes[0])
-# %%
 
-xx = torch.Tensor(im[None])
-xx[0] = 1
+boxes
